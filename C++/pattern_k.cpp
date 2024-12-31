bbc06798aaae36437370456ee8a9951dc5ded0f9
Vishal-Ghosh <vishalghosh2016@gmail.com>
2024-12-31 16:08:59 +0530

    Day 7 Starting to use for loop

---
+++ C++/pattern_k.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter your no: ";
+    cin >> n;
+
+    for (int row = 1; row < 5; row++)
+    {
+        cout << row;
+    }
+
+    return 0;
+}

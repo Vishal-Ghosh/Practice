d6f393722779f937a25091dd55dea22730c7c18a
Vishal-Ghosh <vishalghosh2016@gmail.com>
2025-01-03 22:43:11 +0530

    Day 9 Practicing patterns with for loops

---
+++ C++/pattern_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a no.:  ";
+    cin >> n;
+
+    for (int row = 1; row <= n; row++)
+    {
+        for (int column = 1; column <= n; column++)
+        {
+            cout << row;
+        }
+        cout << endl;
+    }
+    return 0;
+}

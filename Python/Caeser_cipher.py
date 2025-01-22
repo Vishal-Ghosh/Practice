27ff680146fc077752d2c7c18df3892dcba0dc7d
Vishal-Ghosh <vishalghosh2016@gmail.com>
2025-01-22 19:54:41 +0530

    Project Caeser Cipher

---
+++ Python/Caeser_cipher.py
@@ -0,0 +1,46 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""" 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# 2 list helps to solve the index out of range error.
+def caeser(plain_text, shift_no, cipher_direction):
+    ciphered_text=""
+    if cipher_direction == "decode":
+            shift_no *= -1
+    for char in plain_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_no
+            ciphered_text += alphabet[new_position]
+        else:
+            ciphered_text += char         
+    print(f"The {cipher_direction}d text is {ciphered_text}")
+    
+answer = True
+while answer:
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift %= 26
+    caeser(plain_text=text, shift_no= shift, cipher_direction=direction)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no': ")
+    if result == 'no':
+        answer = False
+        print("Goodbye")
+
+
